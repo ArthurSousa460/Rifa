6f5afe74c818963a28639433768094ec79dfb6a7
ArthurSousa460 <arthurprogrammer460@gmail.com>
2025-02-06 22:33:20 -0300

    add /raffel/create endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,15 @@
 import { Request, Response, Router } from "express"
 import ClientController from "./Controllers/ClientController"
+import RaffleController from "./Controllers/RaffleController";
 
 
 const router = Router();
 const clientController = new ClientController();
+const raffleController = new RaffleController()
 
 router.get("/", (req: Request, res: Response) => {res.send("ok")});
 router.post("/client/create", async(req: Request, res: Response) =>{await clientController.createClient(req, res)});
-
+router.post("/raffle/create", async(req: Request, res: Response) => {await raffleController.create(req, res)});
 
 
 
