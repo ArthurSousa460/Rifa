8239d1279439b2e7bc9e7fb1ed422bf8046da449
ArthurSousa460 <arthurprogrammer460@gmail.com>
2025-02-06 21:51:11 -0300

    add Raffle Repository

---
+++ src/repository/RaffleRepository.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+class RaffleRepository{
+    private repository: PrismaClient;
+
+
+    constructor(){
+        this.repository = new PrismaClient();
+    }
+
+    async create(clientId: number){
+        const newRaffle = await this.repository.raffle.create({
+            data:{
+                clientId
+            }
+        })
+        return newRaffle;
+    }
+}
