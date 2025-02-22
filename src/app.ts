34ee6e9d59f4acf66543cdca27c6d2bce74afbc8
root <root@arthur.>
2025-02-22 09:17:19 -0300

    Refactore app.ts and server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,22 @@
-// import express, { Router } from "express"
-// class App {
-//     public server: express.Application;
-//     public router: Router;
+import express, { Router } from "express"
+class App {
+    public server: express.Application;
+    public router: Router;
 
-//     constructor(router: Router){
-//         this.server = express();
-//         this.middleware()
-//         this.router = router
-//         this.setRouter(this.router)
-//     }
+    constructor(router: Router){
+        this.server = express();
+        this.middleware()
+        this.router = router
+        this.setRouter(this.router)
+    }
 
-//     private middleware(){
-//         this.server.use(express.json());
-//     }
+    private middleware(){
+        this.server.use(express.json());
+    }
 
-//     private setRouter(router: Router){
-//         this.server.use(router);
-//     }
-// }
+    private setRouter(router: Router){
+        this.server.use(router);
+    }
+}
 
-// export {App};
-
-import express from "express"
-import router from "./routes";
-
-const app = express();
-app.use(express.json());
-app.use(router)
-
-export default app
+export {App};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import app from "./app";
+import { App } from "./app";
+import router from "./routes";
 
+
+
+const app = new App(router);
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () =>{
-    console.log(`Server runnig ao http://127.0.0.1:${PORT}`);
+
+app.server.listen(PORT, ()=>{
+    console.log(`Server running in http://127.0.0.1:${PORT}`);
 })
